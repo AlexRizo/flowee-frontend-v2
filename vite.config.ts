ab6eed6370d9b81a46db13870ab5bf43b89943c2
devalowee <david_alejandrorizo@outlook.com>
2026-07-09 10:14:17 -0600

    initial config done

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+import babel from '@rolldown/plugin-babel'
+import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    viteReact(),
+    babel({ presets: [reactCompilerPreset()] }),
+  ],
+})
+
+export default config
