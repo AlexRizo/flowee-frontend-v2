9b3c7d69b19ee8b7f8ab115653ab69828cfab87a
devalowee <david_alejandrorizo@outlook.com>
2026-08-11 12:38:43 -0600

    link hover bug fixed

--- src/components/layout/sidebar/nav-spaces.tsx
+++ src/components/layout/sidebar/nav-spaces.tsx
@@ -20,12 +20,14 @@ export const NavSpaces = ({ spaces }: Props) => {
       <SidebarMenu>
         {spaces.map((space) => (
           <Link
+            activeProps={{ className: 'bg-violet-200' }}
+            className='hover:bg-violet-100 rounded-md'
             from="/w/$workspaceCode"
             to="/w/$workspaceCode/s/$spaceCode"
             params={{ spaceCode: space.code }}
           >
             <SidebarMenuItem key={space.id}>
-              <SidebarMenuButton tooltip={space.name}>
+              <SidebarMenuButton tooltip={space.name} className='hover:bg-transparent'>
                 <SpaceIcon color={space.color} name={space.name} />
                 <span>{space.name}</span>
               </SidebarMenuButton>
